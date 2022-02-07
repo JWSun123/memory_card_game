dca0e3e9abc74724136a0a1df5efe60376d45931
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-02-06 20:22:00 -0500

    overlay back and front images. clickable.

--- js/memory.js
+++ js/memory.js
@@ -1,1 +1,7 @@
 //event listener: click a card, switch to the back image.
+
+let front = document.querySelectorAll(".front");
+for (let i = 0; i < front.length; i++){
+    front[i].addEventListener("click", function(){
+        front[i].style.display = "none";
+    })}
