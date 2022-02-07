7f11cc348f95b8156d0b304da87748863f08771b
Yiyi Luo <carol.lyy1214@gmail.com>
2022-02-07 11:33:30 -0500

    difficulty button

--- js/memory.js
+++ js/memory.js
@@ -1,3 +1,18 @@
+//3 levels of diffuculties
+
+$(document).ready(()=>{
+    $('.easy').on('click',function(){
+        $('.hide').hide();
+    })
+    $('.medium').on('click',function(){
+        $('.hide').show();
+        $('.hidden').hide();
+    })
+    $('.hard').on('click',()=>{
+        $('.hide').show();
+    })
+})
+
 //event listener: click a card, switch to the back image.
 
 let front = document.querySelectorAll(".front");
