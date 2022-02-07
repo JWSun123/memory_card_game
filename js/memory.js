0dafccd85c55d6e1d5e431e9d225098b8be11a8e
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-02-07 13:33:32 -0500

    js works, playable now

--- js/memory.js
+++ js/memory.js
@@ -13,10 +13,42 @@ $(document).ready(()=>{
     })
 })
 
+
+
 //event listener: click a card, switch to the back image.
+
+let firstFlip;
+let secondFlip;
+let flipped = false;
+function flip(){
+    this.style.display = "none";
+    if (!flipped){
+        firstFlip = this.nextElementSibling;
+        flipped = true;
+    }
+    else{
+        flipped = false;
+        secondFlip = this.nextElementSibling;
+        isMatch(firstFlip, secondFlip);
+    }
+}
+function disappear(){
+    firstFlip.style.display = "none";
+    secondFlip.style.display = "none";
+}
+function unflip(){
+    firstFlip.previousElementSibling.style.display = "";
+    secondFlip.previousElementSibling.style.display = "";
+}
+function isMatch(firstFlip, secondFlip){
+    if (firstFlip.getAttribute("value") === secondFlip.getAttribute("value")){
+        let timerDisappear = setTimeout(disappear, 800)
+    }
+    else{
+        let timerUnflip = setTimeout(unflip, 800)
+    }
+}
 
 let front = document.querySelectorAll(".front");
 for (let i = 0; i < front.length; i++){
-    front[i].addEventListener("click", function(){
-        front[i].style.display = "none";
-    })}
+    front[i].addEventListener("click", flip)}
