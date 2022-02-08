9d74ceba66c15761be3c2e793acf1c390219932f
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-02-07 20:35:42 -0500

    win()

--- js/memory.js
+++ js/memory.js
@@ -2,24 +2,29 @@
 
 $(document).ready(()=>{
     $('.easy').on('click',function(){
+        resetGame();
         $('.hide').hide();
     })
     $('.medium').on('click',function(){
+        resetGame();
         $('.hide').show();
         $('.hidden').hide();
     })
     $('.hard').on('click',()=>{
+        resetGame();
         $('.hide').show();
     })
 })
+// time used to finish the game. --feel free to change.
+let finishTime;
 
-
-
-//event listener: click a card, switch to the back image.
-
+let front = document.querySelectorAll(".front");
+let back = document.querySelectorAll(".back");
 let firstFlip;
 let secondFlip;
+// variable flipped: if there is already a card flipped: true, else: false.
 let flipped = false;
+let win;
 function flip(){
     this.style.display = "none";
     if (!flipped){
@@ -30,25 +35,76 @@ function flip(){
         flipped = false;
         secondFlip = this.nextElementSibling;
         isMatch(firstFlip, secondFlip);
+        checkWin();
     }
+    
 }
+// if two cards flipped are match, both cards disappear.
 function disappear(){
     firstFlip.style.display = "none";
     secondFlip.style.display = "none";
 }
+// if two cards flipped are not match, both cards unflip.
 function unflip(){
     firstFlip.previousElementSibling.style.display = "";
     secondFlip.previousElementSibling.style.display = "";
 }
+// if the value of two cards are the same, it's match.
 function isMatch(firstFlip, secondFlip){
     if (firstFlip.getAttribute("value") === secondFlip.getAttribute("value")){
-        let timerDisappear = setTimeout(disappear, 800)
+        let timerDisappear = setTimeout(disappear, 300)
     }
     else{
-        let timerUnflip = setTimeout(unflip, 800)
+        let timerUnflip = setTimeout(unflip, 300)
     }
 }
 
-let front = document.querySelectorAll(".front");
+function checkWin(){
+    win = true;
+    for (let i = 0; i < back.length; i++){
+        if (back[i].style.display != "none"){
+            win = false;
+        }
+    }
+    if (win){
+        document.getElementById("congrat").classList.remove("d-none")
+        document.getElementById("result").innerText = "You finish the game in " + finishTime;
+    }
+    console.log(win)
+}
+// a function to shuffle the card randomly
+let cardContainers = document.querySelectorAll(".imageContainer")
+function shuffleCards(){
+    for(let i = 0; i < cardContainers.length; i++){
+        let randomNum = Math.floor(Math.random() * 16);
+        cardContainers[i].style.order = randomNum;
+    }
+}
+
+// a function to reset the game
+function resetGame(){
+    shuffleCards();
+    firstFlip = null;
+    secondFlip = null;
+    flipped = false;
+    document.getElementById("congrat").classList.add("d-none");
+    document.getElementById("result").innerText = "";
+    for (let i = 0; i < front.length; i++){
+        front[i].style.display = ""}
+    for (let i = 0; i < back.length; i++){
+        back[i].style.display = ""}
+}
+
+// event listener: shuffle cards everytime the page is reload.
+ window.addEventListener("load", shuffleCards)
+
+// event listener: click a card, flip it.
 for (let i = 0; i < front.length; i++){
     front[i].addEventListener("click", flip)}
+
+// event listener: reset the game when user clicks START PLAY button.
+let startButton = document.getElementById("startBtn");
+startButton.addEventListener("click", resetGame);
+let againButton = document.getElementById("againBtn");
+againButton.addEventListener("click", resetGame);
+
